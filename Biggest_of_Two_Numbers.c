52d3a5287f2196c2c91792055e056c6fca79e008
Bhagyasree15 <116014283+Bhagyasree15@users.noreply.github.com>
2022-10-27 23:59:02 +0530

    #include <stdio.h> int main() {     int c,d;     scanf("%d%d",&c,&d);     if(c>d)     {         printf("%d",c);     }     else     {         printf("%d",d);     } }

---
+++ Biggest_of_Two_Numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int c,d;
+    scanf("%d%d",&c,&d);
+    if(c>d)
+    {
+        printf("%d",c);
+    }
+    else
+    {
+        printf("%d",d);
+    }
+}    
